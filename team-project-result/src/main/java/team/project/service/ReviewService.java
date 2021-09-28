e0c1b7cdd1dae5ef1c14de8d9f6432b56d588558
양창혁 <82005294+yang-chang-hyuk@users.noreply.github.com>
2021-09-28 10:50:13 +0900

    a

--- team-project-result/src/main/java/team/project/service/ReviewService.java
+++
@@ -1,9 +0,0 @@
-package team.project.service;
-
-import java.util.List;
-
-import team.project.dto.ReviewDto;
-
-public interface ReviewService {
-	List<ReviewDto> getReview(int carIndex) throws Exception;
-}
